d459f432cc06b8892c97e631b0d25af03dab527f
yurixtugal <yurixtugal12@gmail.com>
2022-06-01 15:41:02 -0500

    Commit de definición de entorno

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+const projectID = process.env.INFURA_PROJECT_ID;
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+              rinkeby: {
+                url: 'https://rinkeby.infura.io/v3/${projectID}',
+                accounts: [privateKey]
+              }
+  }
+};
